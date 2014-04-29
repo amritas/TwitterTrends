0396205895269e505712af7a18d7af365f96774d
Amrita <amritash87@gmail.com>
2014-04-29 22:52:30 +0530

    Added code for getting tweets.
    Added twitter library TweetSharp and added the code to get the tweets
    based on a specific keyword.

--- RuleEngine/RuleEngine.cs
+++ RuleEngine/RuleEngine.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using TweetSharp;
+using System.Configuration;
+using Newtonsoft.Json;
 
 namespace RuleEngine
 {
     public class RuleEngineController : ApiController
     {
 
-        public void GetTweets()
+        public string GetTweets(string keyword)
         {
-            
+            var consumerKey = ConfigurationManager.AppSettings["TwitterConsumerKey"];
+            var consumerSecret = ConfigurationManager.AppSettings["TwitterConsumerSecret"];
+            var accessToken = ConfigurationManager.AppSettings["AccessToken"];
+            var accessTokenSecret = ConfigurationManager.AppSettings["AccessTokenSecret"];
+
+            var service = new TwitterService(consumerKey, consumerSecret);
+            service.AuthenticateWith(accessToken, accessTokenSecret);
+
+            var tweets = (service.Search(new SearchOptions { Q = keyword })).Statuses.ToString();
+            return JsonConvert.SerializeObject(tweets);
         }
     }
 }
