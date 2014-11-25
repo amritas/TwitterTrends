f04df2301e6999c90f25f1ac1502f0a4c09c1049
Amrita <amritash87@gmail.com>
2014-11-25 20:47:27 +0530

    Refractored the code

--- Data/Repository.cs
+++ Data/Repository.cs
@@ -18,12 +18,19 @@ namespace Data
             //var client = new MongoClient(ConfigurationManager.AppSettings["connectionString"]);
             //var server = client.GetServer();
 
-            var connectionstring = ConfigurationManager.AppSettings.Get("(MONGOHQ_URL|MONGOLAB_URI)");
+            var connectionstring = GetMongoDbConnectionString();
             var url = new MongoUrl(connectionstring);
             var client = new MongoClient(url);
             var server = client.GetServer();
-            var mongoDb = server.GetDatabase(url.DatabaseName);
-            mongoDb= server.GetDatabase("TwitterTrends");
+            var mongoDb = server.GetDatabase(MongoUrl.Create(connectionstring).DatabaseName);//.GetDatabase(url.DatabaseName);
+            //mongoDb= server.GetDatabase("TwitterTrends");
+        }
+
+        private string GetMongoDbConnectionString()
+        {
+            return ConfigurationManager.AppSettings.Get("MONGOHQ_URL") ??
+            ConfigurationManager.AppSettings.Get("MONGOLAB_URI") ??
+            "mongodb://localhost/APICache";
         }
 
         public void SaveTweets(string tweets)
