37a14ddfa735a010b78679100483ecec731773fe
Amrita <amritash87@gmail.com>
2015-04-19 15:23:59 +0530

    Cleaned the projects

---
+++ Test/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RuleEngine;
+using NUnit;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase]
+        public void GetTweets()
+        {
+            RuleEngineController contrl = new RuleEngineController();
+            contrl.GetLatestTweets();
+            Assert.AreEqual(2, 2);
+        }
+       
+    }
+}
